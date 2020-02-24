fc5d48e14424eb1f33a20c5cb220e9e84f1969cd
cobiebeef <cobiebeef@gmail.com>
2020-02-24 13:50:24 -0500

    Added navigation library to repository

---
+++ Firmware/Libraries/UMSB_NAV/UMSB_NAV.cpp
@@ -0,0 +1,51 @@
+/*!
+ * filename: "nav.cpp"
+ *
+ * purpose: to provide basic navigation functions
+ *
+ * use: include "nav.hpp" and use the functions as described below
+ *
+ * version:
+ *		Alpha 0.00.1
+ *
+ * notes:
+ *		November 12, 2019: need to create a function that takes in a bearing,
+ *			a distance and, an initial cordinate and returns the final cordinate
+ *
+ * contributors:
+ *		Cobie Yung
+ */
+#include "nav.hpp"
+
+namespace nav {
+	/** takes two chordinates and retuns a bearing in degrees **/
+	double bearing(double lat1, double long1, double lat2, double long2) {
+		double dlong = (long2 - long1) * deg_2_rad;
+		lat1 *= deg_2_rad;
+		lat2 *= deg_2_rad;
+		double y = sin(dlong) * cos(lat2);
+		double x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlong);
+		return atan2(y, x) / deg_2_rad;
+	}
+
+	/** takes a latitude and returns the earth's radius in meters at that point **/
+	double earth_radius(double lat) {
+		lat *= deg_2_rad;
+		return sqrt(
+			(pow(pow(EARTH_MAJOR_RADIUS, 2) * cos(lat), 2) + pow(pow(EARTH_MINOR_RADIUS, 2) * sin(lat), 2)) / 
+			(pow(EARTH_MAJOR_RADIUS * cos(lat), 2) + pow(EARTH_MINOR_RADIUS * sin(lat), 2))
+		);
+	}
+
+	/** takes two chordinates and returns the distance between them in meters **/
+	double distance(double lat1, double long1, double lat2, double long2) {
+		double erad = earth_radius((lat1 + lat2) / 2.0);
+		double dlong = (long2 - long1) * deg_2_rad;
+		double dlat = (lat2 - lat1) * deg_2_rad;
+		double a = pow(sin(dlat / 2.0), 2) + cos(lat1 * deg_2_rad) * cos(lat2 * deg_2_rad) * pow(sin(dlong / 2.0), 2);
+		double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+		double d = erad * c;
+
+		return d;
+	}
+}
---
+++ Firmware/Libraries/UMSB_NAV/UMSB_NAV.hpp
@@ -0,0 +1,34 @@
+/*!
+ * filename: "nav.hpp"
+ *
+ * purpose: to provide basic navigation functions
+ *
+ * use: include this file and use the functions as described below
+ *
+ * version:
+ *		Alpha 0.00.1
+ *
+ * notes:
+ *
+ * contributors:
+ *		Cobie Yung
+ */
+
+#ifndef NAV
+#define NAV
+
+#include <math.h>
+
+namespace nav {
+	static constexpr double pi = 3.141592653589793,
+		EARTH_MAJOR_RADIUS = 6378137.0, EARTH_MINOR_RADIUS = 6356752.3,
+		deg_2_rad = pi / 180.0;
+
+	/** takes two chordinates and returns the distance between them in meters **/
+	double distance(double lat1, double long1, double lat2, double long2);
+
+	/** takes two chordinates and retuns a bearing in degrees **/
+	double bearing(double lat1, double long1, double lat2, double long2);
+}
+
+#endif 
