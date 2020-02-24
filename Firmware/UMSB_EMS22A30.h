68404c34b0f12947a60f170c5735686364e92784
Cobie Yung <54954218+cobiebeef@users.noreply.github.com>
2020-02-24 11:54:41 -0500

    Create UMSB_EMS22A30.h

---
+++ Firmware/UMSB_EMS22A30.h
@@ -0,0 +1,25 @@
+/*!
+ * @flie UMSB_EMS22A30.h
+ *
+ * This is a library for the EMS22A30 rotary encoder
+ *
+ * Revision History:
+ *     Version 0.0.01 Initil Commit
+ *
+ * Notes:
+ */
+
+#include "Arduino.h"
+
+#include <SPI.h>
+
+class UMSB_EMS22A30
+{
+public:
+	int ss;
+	UMSB_EMS22A30(int ss = 0);
+	void begin();
+	uint16_t getAngle();
+	uint16_t read();
+	bool parityCheck(uint16_t data);
+};
