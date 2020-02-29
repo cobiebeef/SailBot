7a2dcc6d8af28aea3a0529a469b5861e2717e96f
Cobie Yung <54954218+cobiebeef@users.noreply.github.com>
2020-02-29 07:00:25 -0500

    Improved comments, changed format
    Moved opening curly brackets to be on the same line as the function

--- Firmware/Libraries/UMSB_EMS22A30/UMSB_EMS22A30.cpp
+++ Firmware/Libraries/UMSB_EMS22A30/UMSB_EMS22A30.cpp
@@ -16,19 +16,13 @@
 
 #include "UMSB_EMS22A30.h"
 
-/*!
- * Initializarion
- */
-UMSB_EMS22A30::UMSB_EMS22A30(int pin) 
-{
+/** Initializarion **/
+UMSB_EMS22A30::UMSB_EMS22A30(int pin) {
 	ss = pin;
 }
 
-/*!
- * Read the data from the rotatry encoder
- */
-uint16_t UMSB_EMS22A30::read()
-{
+/** Read the data from the rotatry encoder **/
+uint16_t UMSB_EMS22A30::read() {
 	digitalWrite(ss, LOW);
 	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
 	uint16_t data = SPI.transfer(0x00);
@@ -39,34 +33,23 @@ uint16_t UMSB_EMS22A30::read()
 	return data; 
 }
 
-/*!
- * Get the angle from the EMS22A30
- */
-uint16_t UMSB_EMS22A30::getAngle()
-{
+/** Get the angle from the EMS22A30 **/
+uint16_t UMSB_EMS22A30::getAngle() {
 	uint16_t angle = 0;
 	uint16_t data = read();
 	
-	/** Check for even parity **/
-	if(!parityCheck(data))
-	{
-		/** Grab the first 10 bits **/
-		angle = data >> 6;
+	if(!parityCheck(data)) {  // Check for even parity
+		angle = data >> 6;  // Grab the first 10 bits
 	}
 	
 	return angle;
 }
 
-/*!
- * Check for parity
- * Returns 1 for odd parity, returns 0 for even parity
- */
-bool UMSB_EMS22A30::parityCheck(uint16_t data)
-{
+/** Check for parity; Returns 1 for odd parity, returns 0 for even parity **/
+bool UMSB_EMS22A30::parityCheck(uint16_t data) {
 	bool parity = 0; 
 	
-    while(data) 
-    {
+    while(data) {
         parity = !parity; 
         data = data & (data - 1); 
     }
@@ -74,11 +57,8 @@ bool UMSB_EMS22A30::parityCheck(uint16_t data)
     return parity;
 }
 
-/*!
- * Start up the SPI commuinication protocall
- */
-void UMSB_EMS22A30::begin()
-{
+/** Start up the SPI commuinication protocall **/
+void UMSB_EMS22A30::begin() {
 	SPI.begin();
 	pinMode(ss, OUTPUT);
 	digitalWrite(ss, HIGH);
